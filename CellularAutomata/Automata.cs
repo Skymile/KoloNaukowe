d19c1a810720781f17ace7da63468ee1f694daa4
Patryk Milewski <mpatryk97@gmail.com>
2022-01-26 18:05:43 +0100

    Cellular Automata

---
+++ CellularAutomata/Automata.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CellularAutomata
+{
+	public static class Automata
+	{
+		public static Bitmap Seed(Bitmap bmp)
+		{
+			int stride = bmp.Width * 3;
+
+			int GetPixel(int x, int y) =>
+				x * 3 + y * stride;
+
+			return bmp;
+		}
+	}
+}
---
+++ CellularAutomata/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Media;
+
+namespace CellularAutomata
+{
+	public class MainWindowVM : INotifyPropertyChanged
+	{
+		public ImageSource MainSource
+		{
+			get => this.mainSource;
+			set
+			{
+				this.mainSource = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.MainSource)));
+			}
+		}
+
+		private ImageSource mainSource;
+
+		public event PropertyChangedEventHandler PropertyChanged;
+	}
+
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			var vm = new MainWindowVM();
+			this.DataContext = vm;
+			var bmp = new Bitmap(256, 256);
+			bmp.SetPixel(128, 128, System.Drawing.Color.White);
+			bmp.SetPixel(129, 129, System.Drawing.Color.White);
+			bmp.SetPixel(128, 129, System.Drawing.Color.White);
+			vm.MainSource = bmp.ToSource();
+		}
+	}
+}
