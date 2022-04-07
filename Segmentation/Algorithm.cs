1ee38be4e41c40329fdab337c7c68634f0a65d83
Patryk Milewski <mpatryk97@gmail.com>
2022-04-07 18:00:48 +0200

    Color segmentation

---
+++ Segmentation/Algorithm.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace Segmentation;
+
+// BMP
+// 32 procesory => 4 bajty
+// 7*3 => 21 + 3 => 24 
+// Padding z 0
+// Width * 3
+// 24
+// 2x5
+
+public static class Algorithm
+{
+    // 300x200 % 4 == 0
+    // 301x200 % 4 != 0 //
+    public static BitmapData Normalize(BitmapData bmpData)
+    {
+        int rawStride = bmpData.Stride;
+        int actualStride = bmpData.Width * 3;
+        int difference = rawStride - actualStride;
+
+        int length = bmpData.Width * bmpData.Height * 3;
+
+        var data = new byte[length];
+
+        Marshal.Copy(bmpData.Scan0, data, 0, data.Length);
+
+        for (int y = 0; y < bmpData.Height; y++)
+        {
+            for (int x = 0; x < rawStride; x++)
+            {
+
+                if (x > actualStride)
+                    continue;
+            }
+        }
+
+        Marshal.Copy(data, 0, bmpData.Scan0, data.Length);
+        return bmpData;
+    }
+
+    public static Bitmap Apply(Bitmap bmp, int threshold)
+    {
+        var bmpData = bmp.LockBits(
+            new Rectangle(0, 0, bmp.Width, bmp.Height),
+            ImageLockMode.ReadWrite,
+            PixelFormat.Format24bppRgb
+        );
+
+        int channels = 3;
+        int stride = bmpData.Stride;
+        int length = bmpData.Width * bmpData.Height * 3;
+
+        int[] offsets =
+        {
+            -channels, 
+             channels, 
+                        stride, 
+                      - stride,
+            -channels + stride, 
+            -channels - stride, 
+             channels + stride, 
+             channels - stride
+        };
+
+        var indexToGroup = new Dictionary<int, int>();
+        var groupToColor = new Dictionary<int, byte[]>();
+        int groupCount = 0;
+        var rand = new Random();
+
+        var data = new byte[length];
+
+        Marshal.Copy(bmpData.Scan0, data, 0, data.Length);
+
+        for (int i = 0; i < length; i += channels)
+        {
+            if (indexToGroup.ContainsKey(i))
+                continue;
+
+            var all = new HashSet<int>();
+            var current = new List<int>() { i };
+            var next = new List<int>();
+
+            int value = (data[i] + threshold / 2) / threshold;
+            indexToGroup[i] = groupCount;
+            byte[] rgb = new byte[3];
+            rand.NextBytes(rgb);
+            groupToColor[groupCount] = rgb;
+
+            while (current.Count > 0)
+            {
+                foreach (int k in current)
+                    if (!all.Contains(k))
+                    {
+                        all.Add(k);
+                        indexToGroup[k] = groupCount;
+
+                        foreach (int offset in offsets)
+                        {
+                            int o = k + offset;
+                            if (o > 0 && o < length &&
+                                value == (data[o] + threshold / 2) / threshold)
+                                next.Add(o);
+                        }
+                    }
+                current = next;
+                next = new();
+            }
+
+            ++groupCount;
+        }
+
+        foreach (var i in indexToGroup)
+        {
+            byte[] rgb = groupToColor[i.Value];
+            for (int k = 0; k < channels; k++)
+                data[i.Key + k] = rgb[k];
+        }
+
+        Marshal.Copy(data, 0, bmpData.Scan0, data.Length);
+
+        bmp.UnlockBits(bmpData);
+        return bmp;
+    }
+}
--- Segmentation/MainWindow.xaml.cs
+++ Segmentation/MainWindow.xaml.cs
@@ -1,8 +1,68 @@
 ﻿using System.Drawing;
 using System.Windows;
+using System.Windows.Media;
 
 namespace Segmentation
 {
+    // Segmentacja
+    /// Grupowanie pikseli na bazie spełniania kryteriów
+    /// Grupy pikseli stykające się lokalnie 
+    /// Rozdzielenie obrazu na jakieś logiczne podczęści tło/obiekt
+    // Ekstrakcja
+    /// Crossing Number => Na przykład, czy dany czarny piksel ma jednego czarnego sąsiada. 
+    /// (Zakończenie linii, bo ma tylko 1 sąsiada)
+    // Szkieletyzacja
+    /// K3M, KMM, Zhang-Suen, Zhang-Wang
+    /// Zamiana zbinaryzowanego obrazu na równoważny topologicznie szkielet (linie o grubości 1 piksela)
+    // Binaryzacja
+    /// Zamiana obrazu na obraz 2 wartości (czarne/białe, 0/1)
+    /// Progowanie lokalne 
+    ///     Niblack, Sauvola, Phansalkar, Bersnen
+    /// LuWu, Kapur => na bazie entropii (różnią się +- i minimalna wartość brana a w drugim maksymalna)
+    /// Progowanie globalne => jeden próg na cały obraz 
+    ///     Otsu
+    // Filtry nieliniowe
+    /// Median
+    /// Median Of Medians => Mediana median 
+    ///   3x3 => 3 wektory po 3 piksele, mediana z każdego z wektorów,
+    ///          3 mediany => mediana z 3 wartości
+    // Filtry liniowe
+    /// Rozmycie Gaussa
+    ///   1 2 1
+    ///   2 4 2
+    ///   1 2 1
+    /// 
+    /// Rozmycie polowe (Box Blur)
+    ///   1 1 1
+    ///   1 1 1
+    ///   1 1 1
+    ///   
+    /// Suma > 1
+    ///   
+    /// Laplaciany
+    ///   1  1  1
+    ///   1 -8  1
+    ///   1  1  1
+    ///   
+    ///   0  1  0
+    ///   1 -4  1
+    ///   0  1  0
+    ///   
+    /// 
+    /// Filtr Sobela
+    ///    1  2  1
+    ///    0  0  0
+    ///   -1 -2 -1
+    ///   
+    /// Suma = 0
+    ///
+    /// Filtr wyostrzający
+    ///   1   2  1
+    ///   2  -9  2
+    ///   1   2  1
+    ///   
+    /// Suma = 1
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -11,6 +71,22 @@ namespace Segmentation
         public MainWindow()
         {
             InitializeComponent();
+
+            MainImg.Source = new Bitmap("../../../apple.png")
+                .ToSource();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            MainImg.Source = Algorithm
+                .Apply(new Bitmap("../../../apple.png"), (int)e.NewValue)
+                .ToSource();
+        }
+
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+            MainImg.Source = new Bitmap("../../../apple.png")
+                .ToSource();
         }
     }
 }
