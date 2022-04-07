8e1111d04eff742fc029fcba5889e007a1efc79b
Patryk Milewski <mpatryk97@gmail.com>
2022-04-07 16:05:50 +0200

    Segmentation start

---
+++ Segmentation/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+using System.Windows;
+
+namespace Segmentation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
